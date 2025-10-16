0123f1ebb508326f351eed2f7de36bbf953efe97
golzi99 <golzi99@gmail.com>
2025-10-16 14:11:53 +0300

    add scroll on search page

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { ProfileService } from '../../data/services/profile/profile-service';
-import { Profile } from '../../data/interfaces/profile.interface';
 import { ProfileCard } from '../../common-ui/profile-card/profile-card.component';
 import { ProfileFiltersComponent } from './profile-filters/profile-filters.component';
 
