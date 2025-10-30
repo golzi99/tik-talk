06f6876284227c38dfbf734b9b17037efe3c9a99
golzi99 <golzi99@gmail.com>
2025-10-30 15:40:10 +0300

    moskito and phone input

--- src/app/experimental/lib/form-experimental/form-experimental.component.ts
+++ src/app/experimental/lib/form-experimental/form-experimental.component.ts
@@ -11,11 +11,41 @@ import {
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Address, Features, MockService } from '../mockService/mockService';
 import { KeyValuePipe } from '@angular/common';
+import { DateTime } from 'luxon';
+import { NameValidator } from '../name.validator';
+import { MaskitoOptions, MaskitoPreprocessor } from '@maskito/core';
+import { MaskitoDirective } from '@maskito/angular';
+import {
+  maskitoAddOnFocusPlugin,
+  maskitoCaretGuard,
+  maskitoPrefixPostprocessorGenerator,
+  maskitoRemoveOnBlurPlugin,
+} from '@maskito/kit';
 
 enum ReceiverType {
   PERSON = 'PERSON',
   LEGAL = 'LEGAL',
 }
+
+const phoneRegex = [
+  '+',
+  '7',
+  ' ',
+  '(',
+  /\d/,
+  /\d/,
+  /\d/,
+  ')',
+  ' ',
+  /\d/,
+  /\d/,
+  /\d/,
+  '-',
+  /\d/,
+  /\d/,
+  /\d/,
+  /\d/,
+];
 
 function getAddressForm(initialValues: Address = {}) {
   return new FormGroup({
@@ -34,29 +64,92 @@ function validateStartWith(forbiddenLetter: string): ValidatorFn {
   };
 }
 
+function validateDateRange({
+  fromControlName,
+  toControlName,
+}: {
+  fromControlName: string;
+  toControlName: string;
+}): ValidatorFn {
+  return control => {
+    const fromControl = control.get(fromControlName);
+    const toControl = control.get(toControlName);
+
+    if (!fromControl || !toControl) return null;
+
+    const fromDate = DateTime.fromISO(fromControl.value);
+    const toDate = DateTime.fromISO(toControl.value);
+
+    if (fromDate && toDate && fromDate > toDate) {
+      toControl.setErrors({
+        dateRange: { message: 'Дата начала не может быть позднее даты конца' },
+      });
+      return { dateRange: { message: 'Дата начала не может быть позднее даты конца' } };
+    }
+
+    return null;
+  };
+}
+
+function createCompletePhoneInsertionPreprocessor(): MaskitoPreprocessor {
+  const trimPrefix = (value: string): string => value.replace(/^(\+?7?\s?8?)\s?/, '');
+  const countDigits = (value: string): number => value.replaceAll(/\D/g, '').length;
+
+  return ({ elementState, data }) => {
+    const { value, selection } = elementState;
+
+    return {
+      elementState: {
+        selection,
+        value: countDigits(value) > 11 ? trimPrefix(value) : value,
+      },
+      data: countDigits(data) >= 11 ? trimPrefix(data) : data,
+    };
+  };
+}
+
 @Component({
   selector: 'app-forms-experimental',
-  imports: [ReactiveFormsModule, KeyValuePipe],
+  imports: [ReactiveFormsModule, KeyValuePipe, MaskitoDirective],
   templateUrl: './form-experimental.component.html',
   styleUrl: './form-experimental.component.scss',
 })
 export class FormsExperimentalComponent {
   mockService = inject(MockService);
+  nameValidator = inject(NameValidator);
   features: Features[] = [];
+  readonly maskitoOptions: MaskitoOptions = {
+    mask: phoneRegex,
+    postprocessors: [maskitoPrefixPostprocessorGenerator('+7 ')],
+    plugins: [
+      maskitoAddOnFocusPlugin('+7 '),
+      maskitoRemoveOnBlurPlugin('+7 '),
+      maskitoCaretGuard((value, [from, to]) => [from === to ? '+7 '.length : 0, value.length]),
+    ],
+    preprocessors: [createCompletePhoneInsertionPreprocessor()],
+  };
 
   ReceiverType = ReceiverType;
 
   form = new FormGroup({
     type: new FormControl<ReceiverType>(ReceiverType.PERSON),
-    name: new FormControl<string>('', [Validators.required, validateStartWith('м')]),
+    name: new FormControl<string>('', {
+      validators: [Validators.required],
+      asyncValidators: [this.nameValidator.validate.bind(this.nameValidator)],
+      updateOn: 'blur',
+    }),
     inn: new FormControl<number | null>(null),
     lastName: new FormControl<string>(''),
+    phoneNumber: new FormControl<string>(''),
     addresses: new FormArray([getAddressForm()]),
     feature: new FormRecord({}),
-    dateRange: new FormGroup({
-      from: new FormControl<string>(''),
-      to: new FormControl<string>(''),
-    }),
+    dateRange: new FormGroup(
+      {
+        from: new FormControl<string>(''),
+        to: new FormControl<string>(''),
+      },
+      validateDateRange({ fromControlName: 'from', toControlName: 'to' })
+    ),
   });
 
   constructor() {
---
+++ src/app/experimental/lib/name.validator.ts
@@ -0,0 +1,27 @@
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { delay, map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NameValidator implements AsyncValidator {
+  http = inject(HttpClient);
+
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    return this.http.get<Profile[]>('https://icherniakov.ru/yt-course/account/test_accounts').pipe(
+      delay(1000),
+      map(profiles => {
+        return profiles.filter((profile: Profile) => profile.firstName === control.value).length > 0
+          ? null
+          : {
+              nameValid: {
+                message: `Имя должно быть одинм из спика: ${profiles.map(profile => profile.firstName).join(', ')}`,
+              },
+            };
+      })
+    );
+  }
+}
