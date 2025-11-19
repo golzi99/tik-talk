570f9ee60e22233db75c46e4fdf5fb54e349b02a
golzi99 <golzi99@gmail.com>
2025-11-19 14:53:06 +0300

    add input and address-input

---
+++ libs/common-ui/src/lib/components/addres-input/address-input.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'tt-address-input',
+  imports: [],
+  templateUrl: './address-input.component.html',
+  styleUrl: './address-input.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => AddressInputComponent),
+    },
+  ],
+})
+export class AddressInputComponent implements ControlValueAccessor {
+  writeValue(obj: any): void {}
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {}
+
+  onChange(value: string | null) {}
+
+  onTouched() {}
+}
---
+++ libs/common-ui/src/lib/components/tt-input/tt-input.component.ts
@@ -0,0 +1,58 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  forwardRef,
+  input,
+  signal,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+@Component({
+  selector: 'tt-input',
+  imports: [FormsModule],
+  templateUrl: './tt-input.component.html',
+  styleUrl: './tt-input.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => TtInputComponent),
+    },
+  ],
+})
+export class TtInputComponent implements ControlValueAccessor {
+  type = input<'text' | 'password'>('text');
+  placeholder = input<string>('');
+  disabled = signal<boolean>(false);
+
+  value: string | null = null;
+
+  onChange(value: string | null): void {}
+
+  onTouched() {}
+
+  writeValue(val: string | null): void {
+    this.value = val;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
+
+  onModelChange(val: string | null) {
+    this.onChange(val);
+  }
+}
